5173c31ad11796b66a81610946ae676b8d8d35cf
ADANAN WARISH ASHRAF <adananwarishashraf786@gmail.com>
2024-10-21 09:24:39 +0530

    Add files via upload

---
+++ hiking_2_oApp.swift
@@ -0,0 +1,17 @@
+//
+//  hiking_2_oApp.swift
+//  hiking 2.o
+//
+//  Created by adanan warish ashraf on 20/09/24.
+//
+
+import SwiftUI
+
+@main
+struct hiking_2_oApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
